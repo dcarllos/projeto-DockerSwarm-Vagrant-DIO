58dfab40f3f0b19f1c2d7c3dad68208649dd0e21
Administrador <N@ve2099>
2022-11-02 21:52:38 -0300

    first commit

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-29hd7dhf4unl3d4wytnkpjji905r531sm950q247lqmz7dubxm-4xlys5n8v3yqrup2yh8qov8ic 10.10.10.100:2377
